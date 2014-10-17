b45998758c08031d4489991d8701dbcec7348245
Jennie Ji <jennie.ji@hotmail.com>
2014-10-17 14:24:47 +0800

    Move demo source code to this branch

---
+++ src/app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularCheckgroupApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularCheckgroupApp
+ */
+angular.module('angularCheckgroupSample')
+  .controller('demoController', function ($scope) {
+  	$scope.demoRadio1 = [];
+  });
